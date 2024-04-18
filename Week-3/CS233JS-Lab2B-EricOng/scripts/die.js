731a2b73719a6eaf4b571478e873b25d59071467
Khonon <onge@my.lanecc.edu>
2024-04-17 22:39:02 -0700

    Updated die.js
    - Finished die.js file (received assistance from Brian Bird's code)

--- Week-3/CS233JS-Lab2B-EricOng/scripts/die.js
+++ Week-3/CS233JS-Lab2B-EricOng/scripts/die.js
@@ -5,4 +5,24 @@
     Date: 4/17/2024
  */
 
-console.log('testing');
+class Die {
+
+    //  private variables
+    #value
+
+    //  class constructor
+    constructor() {
+        this.#value = 0;
+    }
+
+    //  getter to get value
+    get value() {return this.#value;}
+
+    //                                                                 //
+    //                      Functions                        //
+    //                                                                 //
+    //  "rolls" a dice to get a random number from 1-6
+    roll() {
+        this.#value = Math.floor(Math.random(1, 6) + 1);    //  remember that +1 is needed since the random number goes to one less than max number
+    }
+}
