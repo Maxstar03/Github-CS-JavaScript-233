e36286d6263095bd608bc64fbd07b1861772c35c
Khonon <onge@my.lanecc.edu>
2024-04-17 22:01:29 -0700

    IDK

---
+++ Week-3/CS233JS-Lab2B-EricOng/scripts/die.js
@@ -0,0 +1,8 @@
+/* 
+    Author: Eric Ong
+    Description: Lab 2B
+    Class: JavaScript 233
+    Date: 4/17/2024
+ */
+
+console.log('testing');
---
+++ Week-3/CS233JS-Lab2B-EricOng/scripts/player.js
@@ -0,0 +1,6 @@
+/* 
+    Author: Eric Ong
+    Description: Lab 2B
+    Class: JavaScript 233
+    Date: 4/17/2024
+ */
